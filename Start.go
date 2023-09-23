1fe3b53815d8be3b1f6379500329576e3e9bb51c
Quach Chi Dung <Dunghuvy@gmail.com>
2023-09-23 13:23:29 +0700

    Date time & Math print test.

--- Start.go
+++ Start.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 func main(){
-	fmt.Println("Hello world!!")	
+
+	dateTime := time.Date(2023, 9, 22, 0, 0, 0, 0, time.Local)
+
+	fmt.Println("Hello world!!")
+	fmt.Println("The time is today is ",dateTime)	
+	fmt.Println("Print rand number \n", rand.Intn(50))
+	fmt.Println("I love you ", math.Sqrt(9.3))
+	fmt.Println(math.Pi)
 }
